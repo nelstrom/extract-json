e0bc175e246f0b0fb976dbc54129d316c1c32e28
Drew Neil <andrew.jr.neil@gmail.com>
2017-06-12 17:38:44 +0100

    Unencode year data

--- convert.rb
+++ convert.rb
@@ -3,7 +3,35 @@ require 'json'
 FileUtils.mkdir_p('data')
 FileUtils.rm_rf(Dir.glob('data/*'))
 
-records = SmarterCSV.process('./2017-data-breaches.csv')
+class YearConverter
+  # Years are encoded
+  # 0  = 2004
+  # 8  = 2012
+  # 9  = 2013
+  # 10 = 2014
+  # 11 = 2015
+  # 12 = 2016
+  # 13 = latest
+  def self.convert(value)
+    {
+      0  => '2004',
+      8  => '2012',
+      9  => '2013',
+      10 => '2014',
+      11 => '2015',
+      12 => '2016',
+      13 => '2017',
+    }[value]
+  end
+end
+
+options = {
+  :value_converters => {
+    :year => YearConverter
+  }
+}
+
+records = SmarterCSV.process('./2017-data-breaches.csv', options)
 records.each_with_index do |record, index|
   filename = (index+1).to_s.rjust(3, "0")
   File.open("data/#{filename}.json", 'a') do |w|
